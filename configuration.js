f3bbb677bbf0ad7dc88694a5f8e95677cf910d79
Isabella Nake <Bella@gess-sipo-als-dock-1-007.ethz.ch>
2012-08-08 14:38:47 +0200

    configuration model,2nd version initialization

---
+++ configuration.js
@@ -0,0 +1,15 @@
+var configurationModel = {
+
+	appId : "",
+	appAuthenticationKey : "",
+	userAuthenticationKey : "",
+	urlToLMS : "",
+	learnerInformation : {
+		userId : "",
+		userName : "",
+		displayName : "",
+		emailAddress : ""
+	},
+	loginState : "loggedOut",
+	globalSynchronizationState : ""
+};
--- main.js
+++ main.js
@@ -1,46 +1,46 @@
 function init() {
+	console.log("0");
 	var connectionState = new ConnectionState();
 	var config = null;
-	var statistics = null;
+	
+	console.log("1");
 
 	if (localStorage.configuration) { // user was already connected to LMS
-		config = JSON.parse(localStorage.configuration);	//take the configuration from the local storage
-	} else {
-		config = {				//create a new configuration
-			appId : "",
-			appAuthenticationKey : "",
-			userAuthenticationKey : "",
-			urlToLMS : "",
-			learnerInformation : {
-				userId : "",
-				userName : "",
-				displayName : "",
-				emailAddress : ""
-			},
-			loginState : "loggedOut",
-			globalSynchronizationState : ""
-		};
+        console.log("1a");
+        config = JSON.parse(localStorage.configuration);	//take the configuration from the local storage
+    } else {
+        console.log("1b");
+		config = configurationModel;	//create new configuration
 	}
 	
-	if (localStorage.statistics) { // statistics already exist
-		config = JSON.parse(localStorage.statistics);	//take the statistics from the local storage
-	} else {
-		statistics = {				//create new statistics
-		};
-	}
+	console.log("2");
 
 	if (connectionState.isOffline()) { // user is offline
+		console.log("3a");
 		if (config.loginState == "loggedOut") {
-			alert("Sorry, you need to be online to connect to your LMS");
+			console.log("Sorry, you need to be online to connect to your LMS");
 		} else if (config.loginState == "loggedIn") {
 			//work with data from local storage
+			config.loginState = "loggedOut";
 		}
 	} else { // user is online
+		console.log("3b");
+        console.log(config.loginState);
 		if (config.loginState == "loggedOut") {
 			// show login
+			console.log("login");
+			config.loginState = "loggedIn";
 		} else if (config.loginState == "loggedIn") {
 			// synchronize data
+			console.log("logout");
+			config.loginState = "loggedOut";
 		}
 	}
+	
+	console.log("4");
+	
+	localStorage.configuration = JSON.stringify(config);
 
+	console.log("5");
+	
 }
