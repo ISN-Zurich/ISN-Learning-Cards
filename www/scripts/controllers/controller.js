6958ebdf94f4450bbd43b241ad79d2417042ea19
Isabella Nake <Bella@gess-sipo-als-dock-1-007.ethz.ch>
2012-08-24 10:30:28 +0200

    splash screen tells controller to switch to the endpoint

--- www/scripts/controllers/controller.js
+++ www/scripts/controllers/controller.js
@@ -1,4 +1,5 @@
-function doNothing() {}
+function doNothing() {
+}
 function openView() {
 	$("#" + this.tagID).show();
 }
@@ -6,110 +7,123 @@ function closeView() {
 	$("#" + this.tagID).hide();
 }
 
-
-function doApologize(){
+function doApologize() {
 	$("#numberInputContainer").hide();
 	$("#correctNumericFeedback").hide();
 	$("#feedbackBody").empty();
 	$("<span/>", {
-	text : "Apologize, no data are loaded"
+		text : "Apologize, no data are loaded"
 	}).appendTo($("#dataErrorMessage"));
-	
 }
 
-
-
-
 function Controller() {
-    var self = this;
+	var self = this;
 
-    console.log("start controller");
-    
-    this.models = {};
-	this.views  = {};
-    
-    this.models.authentication = new ConfigurationModel();
-    this.models.course         = new CourseModel();
-    this.models.questionpool   = new QuestionPoolModel();
-    this.models.answers        = new AnswerModel();
-	
-    console.log("models initialized");
+	console.log("start controller");
+
+	this.models = {};
+	this.views = {};
+
+	this.models.authentication = new ConfigurationModel();
+	this.models.course = new CourseModel();
+	this.models.questionpool = new QuestionPoolModel();
+	this.models.answers = new AnswerModel();
+
+	console.log("models initialized");
 	// this.models['course'].loadData();
-    
-    this.views.splashScreen = new SplashScreen(this);
-    this.views.login        = new LoginView();
-    this.views.logout       = new LogoutView();
-    this.views.coursesList  = new CoursesListView(this);
-    this.views.questionView = new QuestionView();
-    this.views.answerView   = new AnswerView();
-    this.views.feedbackView = new FeedbackView();
-    this.views.settings     = new SettingsView();
-	
-    console.log('views initialized');
-    
+
+	this.views.splashScreen = new SplashScreen(this);
+	this.views.login = new LoginView();
+	this.views.logout = new LogoutView();
+	this.views.coursesList = new CoursesListView(this);
+	this.views.questionView = new QuestionView();
+	this.views.answerView = new AnswerView();
+	this.views.feedbackView = new FeedbackView();
+	this.views.settings = new SettingsView();
+
+	console.log('views initialized');
+
 	this.activeView = this.views['splashScreen'];
-    
-    function swipeCatcher(event) { self.activeView.handleSwipe(event);}
-    function tapCatcher(event) { self.activeView.handleTap(event);}
-    
-    self.jesteroptions = { swipeDistance: 100,
-        avoidFlick: true };
-    
-    var gestureHandler = jester(document, self.jesteroptions)
-    .swipe(swipeCatcher)
-	.tap(tapCatcher);
-    
-    console.log('core gestures done');
-    
-    console.log( 'platform'+ device.platform);
-    if ( device.platform == 'iPhone' ) {
-        
-        function pinchCatcher(event) { self.activeView.handlePinch(event); }
-        gestureHandler.pinched(pinchCatcher);
-    }
-    
-    console.log('initialize endpoint');
-    
-    if (this.models['authentication'].isLoggedIn()) {
-		console.log("is loggedIn");
-		this.transition('coursesList');
-	} else {
-		console.log("is not loggedIn");
-		this.transition('login');
+
+	function swipeCatcher(event) {
+		self.activeView.handleSwipe(event);
 	}
-    
-    
+	function tapCatcher(event) {
+		self.activeView.handleTap(event);
+	}
+
+	self.jesteroptions = {
+		swipeDistance : 100,
+		avoidFlick : true
+	};
+
+	var gestureHandler = jester(document, self.jesteroptions).swipe(
+			swipeCatcher).tap(tapCatcher);
+
+	console.log('core gestures done');
+
+	console.log('platform' + device.platform);
+	if (device.platform == 'iPhone') {
+
+		function pinchCatcher(event) {
+			self.activeView.handlePinch(event);
+		}
+		gestureHandler.pinched(pinchCatcher);
+	}
+	
+	this.activeView.open();
+	
 	console.log("End of Controller");
 } // end of Controller
 
 Controller.prototype.transition = function(viewname) {
-	if ( this.views[viewname] ) {
+	if (this.views[viewname]) {
 		this.activeView.close();
 		this.activeView = this.views[viewname];
 		this.activeView.open();
 	}
 };
 
-Controller.prototype.transitionToLogin = function() { this.transition('login'); };
+Controller.prototype.transitionToEndpoint = function() {
+	console.log('initialize endpoint');
+	
+	if (this.models['authentication'].isLoggedIn()) {
+		console.log("is loggedIn");
+		this.transition('coursesList');
+	} else {
+		console.log("is not loggedIn");
+		this.transition('login');
+	}
+};
 
-Controller.prototype.transitionToLogout = function() { this.transition('logout'); };
+Controller.prototype.transitionToLogin = function() {
+	this.transition('login');
+};
 
+Controller.prototype.transitionToLogout = function() {
+	this.transition('logout');
+};
 
-Controller.prototype.transitionToCourses =  function() { this.transition('coursesList');};
+Controller.prototype.transitionToCourses = function() {
+	this.transition('coursesList');
+};
 
+Controller.prototype.transitionToQuestion = function() {
+	this.transition('questionView');
+};
 
-Controller.prototype.transitionToQuestion = function() { this.transition('questionView');};
+Controller.prototype.transitionToAnswer = function() {
+	this.transition('answerView');
+};
 
+Controller.prototype.transitionToFeedback = function() {
+	this.transition('feedbackView');
+};
 
-Controller.prototype.transitionToAnswer = function() {this.transition('answerView');};
+Controller.prototype.transitionToSettings = function() {
+	this.transition('settings');
+};
 
-Controller.prototype.transitionToFeedback = function() {this.transition('feedbackView');};
-
-Controller.prototype.transitionToSettings = function() {this.transition('settings');};
-
-Controller.prototype.transitionToFeedbackMore = function() {this.transition('feedbackMore');};
-
-
-
-
-
+Controller.prototype.transitionToFeedbackMore = function() {
+	this.transition('feedbackMore');
+};
--- www/scripts/views/splashScreen.js
+++ www/scripts/views/splashScreen.js
@@ -10,6 +10,10 @@ SplashScreen.prototype.handlePinch = doNothing;
 SplashScreen.prototype.handleTap = doNothing;
 SplashScreen.prototype.handleSwipe = doNothing;
 
+SplashScreen.prototype.open = function() {
+	setTimeout(this.controller.transitionToEndpoint(), 2000);
+};
+
 SplashScreen.prototype.closeDiv = closeView;
 
 SplashScreen.prototype.close = function() {
