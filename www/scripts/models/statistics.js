626aaf5bd132238a06a6fc004e76d5db1c7d83e0
Evangelia Mitsopoulou <emitsopo@gess-sipo-als-dock-1-008.ethz.ch>
2012-09-17 15:53:30 +0200

    handledCards function in progress

--- www/scripts/models/statistics.js
+++ www/scripts/models/statistics.js
@@ -7,6 +7,7 @@ function StatisticsModel(controller) {
 	this.db;
 	this.initDB();
 
+	var handledCards;
 };
 
 
@@ -26,36 +27,69 @@ StatisticsModel.prototype.initDB = function() {
 			100000);
 };
 
-StatisticsModel.prototype.loadFromDB = function() {
-	allStatistics = [];
-	this.db.transaction(function(transaction) {
-		transaction.executeSql('SELECT * FROM statistics;', [],
-				dataSelectHandler, function() {/* errorHandler */
-				});
-	});
 
-	function dataSelectHandler(transaction, results) {
-		for ( var i = 0; i < results.rows.length; i++) {
-			row = results.rows.item(i);
-			if (!allStatistics[row['course_id']]) {
-				allStatistics[row['course_id']] = [];
-			}
-			allStatistics[row['course_id']].push({
-				"question" : row['question_id'],
-				"score" : row['score'],
-				"day" : row['day'],
-				"speed" : row['speed']
-			});
-		}
 
-		console.log("Load from DB:");
-		for ( var c in allStatistics) {
-			for ( var q in allStatistics[c]) {
-				console.log("Course: " + c + " Question: "
-						+ allStatistics[c][q].question + " Score: "
-						+ allStatistics[c][q].score);
-			}
-		}
-	}
+StatisticsModel.prototype.getHandledCards = function() {
+
+	this.db.transaction(function(transaction){
+	transaction.executeSql('SELECT COUNT(id) FROM statistics ',[], resultDataHandler,errorHandler);
+});
+	
+	function errorHandler(tx, e){
+		 console.log("error! NOT inserted: " + e.message);	 
+}; 
+
+
+	function resultDataHandler(transaction, results){
+//		//var handledCards [];
+//		//count =0;
+//		//for (var i=0; i< results.rows.length; i++){	
+var row = results.rows.length;
+//		//row = results.rows.item(0);	
+	console.log("number of handled cards:" +row);
+	handledCards = row;
+//			//count++; 
+		
+//		//}
+//		//return count;
+	};
 
 };
+
+
+
+
+
+//StatisticsModel.prototype.loadFromDB = function() {
+//	allStatistics = [];
+//	this.db.transaction(function(transaction) {
+//		transaction.executeSql('SELECT * FROM statistics;', [],
+//				dataSelectHandler, function() {/* errorHandler */
+//				});
+//	});
+//
+//	function dataSelectHandler(transaction, results) {
+//		for ( var i = 0; i < results.rows.length; i++) {
+//			row = results.rows.item(i);
+//			if (!allStatistics[row['course_id']]) {
+//				allStatistics[row['course_id']] = [];
+//			}
+//			allStatistics[row['course_id']].push({
+//				"question" : row['question_id'],
+//				"score" : row['score'],
+//				"day" : row['day'],
+//				"speed" : row['speed']
+//			});
+//		}
+//
+//		console.log("Load from DB:");
+//		for ( var c in allStatistics) {
+//			for ( var q in allStatistics[c]) {
+//				console.log("Course: " + c + " Question: "
+//						+ allStatistics[c][q].question + " Score: "
+//						+ allStatistics[c][q].score);
+//			}
+//		}
+//	}
+//
+//};
