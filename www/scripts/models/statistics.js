42ad4dfa2d8af9fd95be3b004eba9b460690aaf5
Isabella Nake <Bella@gess-sipo-als-dock-1-007.ethz.ch>
2012-09-19 13:45:16 +0200

    statistics achievements

--- www/scripts/models/statistics.js
+++ www/scripts/models/statistics.js
@@ -16,6 +16,8 @@ function StatisticsModel(controller) {
 	this.statistics['averageSpeed'] = -1;
 	this.statistics['bestDay'];
 	this.statistics['bestScore'] = -1;
+	this.statistics['stackHandler'] = -1;
+	this.statistics['cardBurner']
 
 	this.improvement = [];
 	this.improvement['handledCards'] = 0;
@@ -70,6 +72,7 @@ StatisticsModel.prototype.initQueries = function() {
 	this.queries['handledCards'] = {query: "", values: [], values24hBefore: []};
 	this.queries['progress'] = {query: "", values: [], values24hBefore: []};
 	this.queries['best'] = {query: "", values: [], values24hBefore: []};
+	this.queries['stackHandler'] = {query: "", values: [], values24hBefore: []};
 	
 	// average score
 	this.queries['avgScore'].query = 'SELECT sum(score) as score, count(id) as num FROM statistics WHERE course_id=?'
@@ -90,6 +93,9 @@ StatisticsModel.prototype.initQueries = function() {
 	this.queries['best'].query = "SELECT DATE(day/1000, 'unixepoch') as day, sum(score) as score, count(id) as num"
 			+ " FROM statistics WHERE course_id=?"
 			+ " GROUP BY DATE(day/1000, 'unixepoch')";
+	
+	// stack handler
+	this.queries['stackHandler'].query = 'SELECT DISTINCT question_id FROM statistics WHERE course_id=?';
 };
 
 StatisticsModel.prototype.initQueryValues = function() {
@@ -117,6 +123,9 @@ StatisticsModel.prototype.initQueryValues = function() {
 
 	// best day and score
 	this.queries['best'].values = [ this.currentCourseId ];
+	
+	// stack handler
+	this.queries['stackHandler'].values = [ this.currentCourseId ];
 };
 
 StatisticsModel.prototype.calculateValues = function() {
@@ -138,6 +147,9 @@ StatisticsModel.prototype.calculateValues = function() {
 
 	// calculate best day and score
 	self.getDataFromDB(self.queries['best'].query, self.queries['best'].values, self.calculateBestDayAndScore);
+	
+	// calculate stack handler
+	self.getDataFromDB(self.queries['stackHandler'].query, self.queries['stackHandler'].values, self.calculateStackHandler);
 };
 
 StatisticsModel.prototype.getDataFromDB = function(query, values, cbResult) {
@@ -154,6 +166,12 @@ StatisticsModel.prototype.calculateHandledCards = function(statisticsModel, tran
 		var row = results.rows.item(0);
 		console.log("number of handled cards:" + row['c']);
 		self.statistics['handledCards'] = row['c'];
+		if (row['c'] > 100) {
+			self.statistics['cardBurner'] = 100;
+		} else {
+			self.statistics['cardBurner'] = row['c'];
+		}
+		console.log("card burner: " + self.statistics['cardBurner']);
 		
 		// calculate improvement
 		self.getDataFromDB(self.queries['handledCards'].query, self.queries['handledCards'].values24hBefore, self.calculateImprovementHandledCards);
@@ -286,7 +304,7 @@ StatisticsModel.prototype.calculateImprovementProgress = function(statisticsMode
 };
 
 StatisticsModel.prototype.calculateImprovementProgressHelperFunction = function(statisticsModel, transaction, 
-																			results, numCorrect) {
+		results, numCorrect) {
 	var self = statisticsModel;
 	if (results.rows.length > 0) {
 		var row = results.rows.item(0);
@@ -299,6 +317,26 @@ StatisticsModel.prototype.calculateImprovementProgressHelperFunction = function(
 		$(document).trigger("statisticcalculationsdone");
 	}
 };
+
+StatisticsModel.prototype.calculateStackHandler = function(statisticsModel, 
+		transaction, results) {
+	var self = statisticsModel;
+	allCards = controller.models["questionpool"].questionList;
+	handledCards = [];
+	numHandledCards = 0;
+	for ( var i = 0; i < results.rows.length; i++) {
+		row = results.rows.item(i);
+		handledCards.push(row['question_id']);
+	}
+	for (var a in allCards) {
+		if (handledCards.indexOf(allCards[a].id) != -1) {
+			numHandledCards++;
+		}
+	}
+	numAllCards = allCards.length;
+	self.statistics['stackHandler'] = Math.round((numHandledCards/numAllCards)*100);
+	console.log("stackHandler: " + self.statistics['stackHandler'] + " handled: " + numHandledCards + " all: " + numAllCards);
+}
 
 StatisticsModel.prototype.dbErrorFunction = function(tx, e) {
 	console.log("DB Error: " + e.message);
