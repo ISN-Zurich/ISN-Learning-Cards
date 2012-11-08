39da7d65edfa67e3f5716dfb58c2cd4b5a4e5e0a
Evangelia Mitsopoulou <emitsopo@gess-sipo-als-dock-1-008.ethz.ch>
2012-11-08 18:12:53 +0100

    Controller documentation

--- www/scripts/controllers/controller.js
+++ www/scripts/controllers/controller.js
@@ -1,6 +1,7 @@
 
 /**	THIS COMMENT MUST NOT BE REMOVED
 
+
 Licensed to the Apache Software Foundation (ASF) under one
 or more contributor license agreements.  See the NOTICE file 
 distributed with this work for additional information
@@ -18,15 +19,37 @@ KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.	
 
-*/
+ */
 
 /** @author Isabella Nake
  * @author Evangelia Mitsopoulou
  */
 
+
 /*jslint vars: true, sloppy: true */
 
+
+/**
+ *A global property/variable that activates and deactivates the display of console logs.
+ *It is passed as parameter in global function moblerlog in common.js.
+ *
+ *@property MOBLERDEBUG
+ *@default 0
+ *
+ **/
+
 var MOBLERDEBUG = 0;
+
+/**
+ * @class Controller
+ * It facilitates the communication between views and models.It handles the display of the views.
+ * @constructor 
+ * it creates the controller, it initializes the  models, views and user interface language 
+ * it specifies a timer of 3ms until the application reaches its starting point,
+ * it handles general swipe and tap gestures that are applicable in each view of the application, 
+ * it handles the change of height of specific buttons according to orientation change, 
+ * it handles the transition to statistics view by listening to the event that is fired when all statistics calculations are done
+ */
 
 function Controller() {
 	var self = this;
@@ -41,76 +64,86 @@ function Controller() {
 	this.views = {};
 
 	// initialize models
-	
+
 	this.models.authentication = new ConfigurationModel(this);
 	this.models.course = new CourseModel(this);
 	this.models.questionpool = new QuestionPoolModel(this);
 	this.models.answers = new AnswerModel(this);
 	this.models.statistics = new StatisticsModel(this);
 	this.models.tracking = new TrackingModel(this);
-    
-    // add synchronization triggers at the end of the model initialization just to be careful 
-    this.models.connection = new ConnectionState(this);
-	
+
+	// add synchronization triggers at the end of the model initialization just to be careful 
+	this.models.connection = new ConnectionState(this);
+
 	this.models.authentication.loadFromServer();
-	
-    	moblerlog("models initialized");
-	
+
+	moblerlog("models initialized");
+
 	//initialize user interface language
 	this.setupLanguage();
 
-    moblerlog('languages are set up');
+	moblerlog('languages are set up');
 
 	// initialize views
 	this.views.splashScreen = new SplashScreen(this);
 	this.views.login = new LoginView(this);
 	this.views.logout = new LogoutView();
-    this.views.coursesList = new CoursesListView(this);
-    this.views.questionView = new QuestionView();
-    this.views.answerView = new AnswerView();
-    this.views.feedbackView = new FeedbackView();
-    this.views.settings = new SettingsView();
-    this.views.statisticsView = new StatisticsView(this);
-    this.views.achievements = new AchievementsView();
-    this.views.about = new AboutView();
+	this.views.coursesList = new CoursesListView(this);
+	this.views.questionView = new QuestionView();
+	this.views.answerView = new AnswerView();
+	this.views.feedbackView = new FeedbackView();
+	this.views.settings = new SettingsView();
+	this.views.statisticsView = new StatisticsView(this);
+	this.views.achievements = new AchievementsView();
+	this.views.about = new AboutView();
 
-    moblerlog('views initialized');
+	moblerlog('views initialized');
 
 	this.activeView = this.views.splashScreen;
+
+	/**
+	 * A handler that is executed when a swipe gesture is detected on the active view
+	 * @function swipeCatcher
+	 * @param  swipe event that is captured by jester
+	 */	
 
 	function swipeCatcher(event) {
 		self.activeView.handleSwipe(event);
 	}
-	
+
+	/**
+	 * A handler that is executed when a tap gesture is detected on the active view
+	 * @function tapCatcher
+	 * @param  tap event that is captured by jester
+	 */	
+
 	function tapCatcher(event) {
 		self.activeView.handleTap(event);
 	}
-	
 
 
+	//define jester options that affect the way gestures are recognized
 	self.jesteroptions = {
-		swipeDistance : 100,
-		avoidFlick : true
+			swipeDistance : 100,
+			avoidFlick : true
 	};
 
-	
-//	var gestureHandler = jester(document, self.jesteroptions).swipe(
-//			swipeCatcher).tap(tapCatcher);
-	
+	//implementation of basic jester syntax in order to recognize swipe and tap events
+
 	var gestureHandler = jester(window, self.jesteroptions).swipe(
 			swipeCatcher).tap(tapCatcher);
 
-	
+
 	moblerlog('core gestures done');
 
 	// if device is an iPhone enable pinching
-    moblerlog('platform' + device.platform);
+	moblerlog('platform' + device.platform);
 	if (device.platform === 'iPhone') {
 
-		
+
 		gestureHandler.pinched(function pinchCatcher(event) {
-                               self.activeView.handlePinch(event);
-                               });
+			self.activeView.handlePinch(event);
+		});
 	}
 
 	// set correct height of icon button
@@ -119,16 +152,22 @@ function Controller() {
 
 	setButtonHeight();
 
+	/**
+	 * It is triggered in Statistics Model when calculations are done in all statistics sub models.  
+	 * @event allstatisticcalculationsdone
+	 * @param: a callback function that executes the transition to statistics view when the event is listened.
+	 * */
+	
 	$(document).bind("allstatisticcalculationsdone", function() {
 		self.transition('statisticsView');
-    });
-	
-	
+	});
+
+		
 	// check if 3000 ms have passed
 	// if not we wait until 3000 ms have passed
 	// then we do the transition to the login view
 	// the remaining waiting time is 3000 - deltatime
-	
+
 	var currentTime = new Date().getTime();
 	var deltaTime= currentTime - startTime;
 	if (deltaTime < 3000) {
@@ -137,71 +176,81 @@ function Controller() {
 	else {
 		self.transitionToEndpoint();
 	}
-	
-	
+
+
 	moblerlog("End of Controller");
 } // end of Controller
 
 
-//sets the correct strings depending on the language
+/**
+ * Initial Interface logic localization. Sets the correct strings depending on the language that is specified in the configuration model.
+ * Make use of i18n jQuery plugin and apply its syntax for localization. 
+ * @prototype setupLanguage 
+ * */
+ 
 
 Controller.prototype.setupLanguage = function() {
 	jQuery.i18n.properties({
-	    name:'textualStrings', 
-	    path:'translations/', 
-	    mode:'both',
-	    language:this.models.authentication.getLanguage(), 
-	    callback: function() { // initialize the static strings on all views
-	        $("#usernameInput").attr("placeholder", msg_placeholder_username);
-	        $("#password").attr("placeholder", msg_placeholder_password);
-	        $("#coursesListTitle").text(msg_courses_list_title);
-	        $("#settingsTitle").text(msg_settings_title);
-	        $("#logoutConfirmationTitle").text(msg_logout_title);
-	        $("#statBestDayTitle").text(msg_bestDay_title);
-	        $("#statBestScoreTitle").text(msg_bestScore_title);
-	        $("#statsBestScoreInfo").text(msg_bestScore_info);
-	        $("#statHandledCardsTitle").text(msg_handledCards_title);
-	        $("#statAverageScoreTitle").text(msg_averageScore_title);
-	        $("#statProgressTitle").text(msg_progress_title);
-	        $("#statsProgressInfo").text(msg_progress_info);
-	        $("#statSpeedTitle").text(msg_speed_title);
-	        $("#statsSpeedinfo").text(msg_speed_info);
-	        $("#achievementsTitle").text(msg_achievements_title);
-	        $("#stackHandlerIcon").addClass(msg_achievements_Handler_icon);
-	        $("#stackHandlerTitle").text(msg_achievementsHandler_title);
-	        $("#stackHandlerExplanation").text(msg_achievementsHandler_explanation);
-	        $("#starterStackHandler").text(msg_achievements_text1);
-	        $("#doneStackHandler").text(msg_achievements_text2);
-	        $("#cardBurnerIcon").addClass(msg_achievements_Burner_icon);
-	        $("#cardBurnerTitle").text(msg_achievementsBurner_title);
-	        $("#cardBurnerExplanation").text(msg_achievementsBurner_explanation);
-	        $("#starterCardBurner").text(msg_achievements_text1);
-	        $("#doneCardBurner").text(msg_achievements_text2);
-	        $("#aboutTitle").text(msg_about_title);
-	        $(".cardBody").text(msg_logout_body);
-	        $("#nameLabelSet").text(jQuery.i18n.prop('msg_fullname'));
-	        $("#usernameLabelSet").text(jQuery.i18n.prop('msg_username'));
-	        $("#emailLabelSet").text(jQuery.i18n.prop('msg_email'));
-	        $("#languageLabelSet").text(jQuery.i18n.prop('msg_language'));
-	        $("#copyright").text(jQuery.i18n.prop('msg_copyright'));
-	        $("#openSource").text(jQuery.i18n.prop('msg_openSource'));
-	        $("#license").text(jQuery.i18n.prop('msg_license'));
-	        $("#cardQuestionTitle").text(jQuery.i18n.prop('msg_question_title'));
-	        
-	        
-	    }
-                           });
+		name:'textualStrings', 
+		path:'translations/', 
+		mode:'both',
+		language:this.models.authentication.getLanguage(), 
+		callback: function() { // initialize the static strings on all views
+			$("#usernameInput").attr("placeholder", msg_placeholder_username);
+			$("#password").attr("placeholder", msg_placeholder_password);
+			$("#coursesListTitle").text(msg_courses_list_title);
+			$("#settingsTitle").text(msg_settings_title);
+			$("#logoutConfirmationTitle").text(msg_logout_title);
+			$("#statBestDayTitle").text(msg_bestDay_title);
+			$("#statBestScoreTitle").text(msg_bestScore_title);
+			$("#statsBestScoreInfo").text(msg_bestScore_info);
+			$("#statHandledCardsTitle").text(msg_handledCards_title);
+			$("#statAverageScoreTitle").text(msg_averageScore_title);
+			$("#statProgressTitle").text(msg_progress_title);
+			$("#statsProgressInfo").text(msg_progress_info);
+			$("#statSpeedTitle").text(msg_speed_title);
+			$("#statsSpeedinfo").text(msg_speed_info);
+			$("#achievementsTitle").text(msg_achievements_title);
+			$("#stackHandlerIcon").addClass(msg_achievements_Handler_icon);
+			$("#stackHandlerTitle").text(msg_achievementsHandler_title);
+			$("#stackHandlerExplanation").text(msg_achievementsHandler_explanation);
+			$("#starterStackHandler").text(msg_achievements_text1);
+			$("#doneStackHandler").text(msg_achievements_text2);
+			$("#cardBurnerIcon").addClass(msg_achievements_Burner_icon);
+			$("#cardBurnerTitle").text(msg_achievementsBurner_title);
+			$("#cardBurnerExplanation").text(msg_achievementsBurner_explanation);
+			$("#starterCardBurner").text(msg_achievements_text1);
+			$("#doneCardBurner").text(msg_achievements_text2);
+			$("#aboutTitle").text(msg_about_title);
+			$(".cardBody").text(msg_logout_body);
+			$("#nameLabelSet").text(jQuery.i18n.prop('msg_fullname'));
+			$("#usernameLabelSet").text(jQuery.i18n.prop('msg_username'));
+			$("#emailLabelSet").text(jQuery.i18n.prop('msg_email'));
+			$("#languageLabelSet").text(jQuery.i18n.prop('msg_language'));
+			$("#copyright").text(jQuery.i18n.prop('msg_copyright'));
+			$("#openSource").text(jQuery.i18n.prop('msg_openSource'));
+			$("#license").text(jQuery.i18n.prop('msg_license'));
+			$("#cardQuestionTitle").text(jQuery.i18n.prop('msg_question_title'));
+
+
+		}
+	});
 };
 
 
-//closes the current view and opens the specified one
- 
+
+/**
+ * Closes the current view and opens the specified one
+ * @prototype
+ * @function transition 
+ * @param {String} viewname, the name of the specified target view
+ **/
+
+
 Controller.prototype.transition = function(viewname) {
-	//if (this.views[viewname]) {
 	moblerlog("transition start" );
-	if (this.views[viewname] && 
-			( viewname === "login" ||
-			this.activeView.tagID !== this.views[viewname].tagID)){
+	// Check if the current active view exists and either if it is different from the targeted view or if it is the login view
+	if (this.views[viewname] && ( viewname === "login" || this.activeView.tagID !== this.views[viewname].tagID)){
 		moblerlog("transition: yes we can!");
 		this.activeView.close();
 		this.activeView = this.views[viewname];
@@ -209,12 +258,16 @@ Controller.prototype.transition = function(viewname) {
 	}
 };
 
-/* if user is already login, the course list is shown otherwise the login form
-  is shown */
+
+/**
+ * It  navigates to the first view that is shown after the the constructor has been initialized and has reached its end point.
+ * If a user is already logged in, the course list is shown otherwise the login form is shown.
+ * @prototype
+ * @function transitionToEndpoint 
+ **/
 
 Controller.prototype.transitionToEndpoint = function() {
 	moblerlog('initialize endpoint');
-
 	if (this.models['authentication'].isLoggedIn()) {
 		moblerlog("is loggedIn");
 		this.transition('coursesList');
@@ -225,15 +278,35 @@ Controller.prototype.transitionToEndpoint = function() {
 };
 
 
-//transition to the specified view
- 
+
+/**
+ * Transition to login view.
+ * @prototype
+ * @function transitionToLogin 
+ **/
+
 Controller.prototype.transitionToLogin = function() {
 	this.transition('login');
 };
 
+/**
+ * Transition to logout view
+ * @prototype
+ * @function transitionToLogout 
+ **/
+
 Controller.prototype.transitionToLogout = function() {
 	this.transition('logout');
 };
+
+
+/**
+ * Helper function that handles the transition to the specified targeted view by firstly checking if the user is logged in. 
+ * If the user is not logged in the transition reaches the login view
+ * @prototype
+ * @function transitionToAuthArea 
+ * @param {String} viewname, the name of the targeted view
+ **/
 
 Controller.prototype.transitionToAuthArea = function(viewname) {
 	if (this.getLoginState()) {
@@ -244,33 +317,77 @@ Controller.prototype.transitionToAuthArea = function(viewname) {
 	}
 };
 
+/**
+ * Transition to courses list view
+ * @prototype
+ * @function transitionToCourses 
+ **/
+
+
 Controller.prototype.transitionToCourses = function() {
 	this.transitionToAuthArea('coursesList');
 };
+
+/**
+ * Transition to question view
+ * @prototype
+ * @function transitionToQuestion 
+ **/
+
 
 Controller.prototype.transitionToQuestion = function() {
 	this.transitionToAuthArea('questionView');
 };
 
+/**
+ * Transition to answer view
+ * @prototype
+ * @function transitionToAnswer 
+ **/
+
 Controller.prototype.transitionToAnswer = function() {
 	this.transitionToAuthArea('answerView');
 };
+
+/**
+ * Transition to feedback view
+ * @prototype
+ * @function transitionToFeedback 
+ **/
 
 Controller.prototype.transitionToFeedback = function() {
 	this.transitionToAuthArea('feedbackView');
 };
 
+/**
+ * Transition to settings view
+ * @prototype
+ * @function transitionToSettings 
+ **/
+
 Controller.prototype.transitionToSettings = function() {
 	this.transitionToAuthArea('settings');
 };
+
+/**
+ * Transition to feedback more view, which is the view that contains any extra tips about the feedback.
+ * @prototype
+ * @function transitionToFeedbackMore 
+ **/
 
 Controller.prototype.transitionToFeedbackMore = function() {
 	this.transitionToAuthArea('feedbackMore');
 };
 
+/**
+ * Transition to statistics view. The user can reach the statistics view in two ways: 1) either by clicking the statistics icon on the course list view or  2) from the achievements view.
+ * @prototype
+ * @function transitionToStatistics 
+ **/
+
 Controller.prototype.transitionToStatistics = function(courseID) {
-	if ( this.getLoginState() ) {
-			
+	if (this.getLoginState() ) {
+		//The transition to statistics view is done by clicking the statistics icon in the course list view. In this case a courseID is assigned for the clicked option.
 		if (courseID && courseID > 0) {
 			this.models['statistics'].setCurrentCourseId(courseID);
 		}
@@ -284,23 +401,55 @@ Controller.prototype.transitionToStatistics = function(courseID) {
 	else {
 		this.transition("login");
 	}
-};
+
+/**
+ * Transition to achievements view
+ * @prototype
+ * @function transitionToAchievements 
+ **/
 
 Controller.prototype.transitionToAchievements = function() {
 	this.transitionToAuthArea('achievements');
 };
 
+/**
+ * Transition to about view
+ * @prototype
+ * @function transitionToAbout 
+ **/
+
 Controller.prototype.transitionToAbout = function() {
 	this.transitionToAuthArea('about');
 };
+
+/**
+ * @prototype
+ * @function getLoginState
+ * @return {boolean} true if the user is logged in (he has an authentication key stored in the local storage) and false if not.
+ **/
 
 Controller.prototype.getLoginState = function() {
 	return this.models["authentication"].isLoggedIn();
 };
 
+
+/**
+ * @prototype
+ * @function getConfigVariable
+ * @param {String} varname, the name of the 
+ * @return {String} It returns the name of the added property of the configuration object. 
+ **/
+
 Controller.prototype.getConfigVariable = function(varname) {
 	return this.models["authentication"].configuration[varname];
 };
+
+/**
+ * It adds a property in the local storage for the configuration object and assigns a value to it.
+ * @prototype
+ * @function setConfigVariable
+ * @param {String} varname, {Boolean, String} varvalue
+ **/
 
 Controller.prototype.setConfigVariable = function(varname, varvalue) {
 	if ( !this.models["authentication"].configuration ) {
@@ -311,13 +460,21 @@ Controller.prototype.setConfigVariable = function(varname, varvalue) {
 };
 
 
-//sets the current height for icon buttons
+/**
+ * Sets the current height for the icon buttons.
+ * Expands the height of the "done" buttons to reach the height of the main content area of the active view
+ * @prototype
+ * @function setButtonHeight
+ **/
 
 function setButtonHeight() {
 	moblerlog("setButtonHeight");
 	var height, windowheight = $(window).height();
 	
-	if (windowheight > 61) {
+	//The main content area has a top margin of 55px. 
+	//The icon button can be extended below the title area if the window height is bigger than 61px (safe value, a bit bigger than 55). 
+	//In this case the button's height will be the difference between the height of the window and the 61 px.  
+	if (windowheight > 61) { 
 		height = windowheight - 61;
 	} else {
 		height = windowheight;
