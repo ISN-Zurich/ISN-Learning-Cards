0302e059e9650f7ef00286ee522423b008bc0515
Evangelia Mitsopoulou <emitsopo@gess-sipo-als-dock-1-008.ethz.ch>
2013-03-25 16:18:02 +0100

    sanity check in landing view before do the transition to question view of featured content

--- www/scripts/views/landingView.js
+++ www/scripts/views/landingView.js
@@ -237,14 +237,14 @@ LandingView.prototype.clickFeaturedItem = function(featuredCourseId){
 	//if (this.controller.models['featured'].isSynchronized()) {
 	this.controller.models['questionpool'].reset();
 	this.controller.models['questionpool'].loadData(featuredCourseId);
-	//if (this.controller.models.questionpool.dataAvailble()) {
+	if (this.controller.models.questionpool.dataAvailable()) {
 		this.controller.models['answers'].setCurrentCourseId(featuredCourseId);
 		moblerlog("enters clickFeauturedItem");
 		this.controller.transitionToQuestion(featuredCourseId);
-	//}
-	//else {
+	}
+	else {
 		// inform the user that something went wrong. 
-	//}
+	}
 	//}
 };
 
