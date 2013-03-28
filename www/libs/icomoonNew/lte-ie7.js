7d894675da8c27d7c9136379d6f682ee82adfe7a
Evangelia Mitsopoulou <emitsopo@gess-sipo-als-dock-1-008.ethz.ch>
2013-03-28 18:52:30 +0100

    added new iconmmon folder

--- www/libs/icomoonNew/lte-ie7.js
+++ www/libs/icomoonNew/lte-ie7.js
@@ -1,4 +1,4 @@
-/* Use this script if you need to support IE 7 and IE 6. */
+/* Load this script using conditional IE comments if you need to support IE 7 and IE 6. */
 
 window.onload = function() {
 	function addIcon(el, entity) {
@@ -6,33 +6,34 @@ window.onload = function() {
 		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
 	}
 	var icons = {
-			'icon-diamond' : '&#xe039;',
-			'icon-smiley' : '&#xe002;',
-			'icon-happy' : '&#xe003;',
-			'icon-arrow-down' : '&#xe004;',
-			'icon-settings' : '&#xe006;',
-			'icon-question' : '&#xe007;',
-			'icon-numeric' : '&#xe008;',
-			'icon-neutral' : '&#xe009;',
-			'icon-lock' : '&#xe00a;',
-			'icon-exit' : '&#xe00b;',
-			'icon-equals' : '&#xe00c;',
-			'icon-enter' : '&#xe00d;',
-			'icon-drag' : '&#xe00e;',
-			'icon-dash' : '&#xe00f;',
-			'icon-cross' : '&#xe010;',
-			'icon-loadingOld' : '&#xe011;',
-			'icon-loading2' : '&#xe012;',
-			'icon-courses' : '&#xe013;',
-			'icon-checkmark2' : '&#xe014;',
-			'icon-lms' : '&#xe018;',
-			'icon-checkmark' : '&#xe019;',
-			'icon-loading' : '&#xe01a;',
-			'icon-info' : '&#xe01b;',
-			'icon-bars' : '&#xe01c;',
-			'icon-icon-move' : '&#xe01d;',
-			'icon-arrow-up' : '&#xe01e;',
-			'icon-fire' : '&#xe038;'
+			'icon-smiley' : '&#xe000;',
+			'icon-checkmark2' : '&#xe001;',
+			'icon-settings' : '&#xe002;',
+			'icon-checkmark' : '&#xe003;',
+			'icon-question' : '&#xe004;',
+			'icon-bars' : '&#xe005;',
+			'icon-numeric' : '&#xe006;',
+			'icon-arrow-up' : '&#xe007;',
+			'icon-neutral' : '&#xe008;',
+			'icon-lock' : '&#xe009;',
+			'icon-arrow-down' : '&#xe00a;',
+			'icon-loadingOld' : '&#xe00b;',
+			'icon-loading2' : '&#xe00c;',
+			'icon-loading' : '&#xe00d;',
+			'icon-lms' : '&#xe00e;',
+			'icon-info' : '&#xe00f;',
+			'icon-icon-move' : '&#xe010;',
+			'icon-happy' : '&#xe011;',
+			'icon-exit' : '&#xe012;',
+			'icon-equals' : '&#xe013;',
+			'icon-enter' : '&#xe014;',
+			'icon-drag' : '&#xe015;',
+			'icon-dash' : '&#xe016;',
+			'icon-cross' : '&#xe017;',
+			'icon-courses' : '&#xe018;',
+			'icon-clozeQuestion' : '&#xe019;',
+			'icon-fire' : '&#xe01a;',
+			'icon-diamonds' : '&#xe01b;'
 		},
 		els = document.getElementsByTagName('*'),
 		i, attr, html, c, el;
