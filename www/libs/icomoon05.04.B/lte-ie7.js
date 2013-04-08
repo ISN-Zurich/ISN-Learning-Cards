3454eac2522b536edb9341d8d4ef249dc6bd4cd6
Evangelia Mitsopoulou <emitsopo@gess-sipo-als-dock-1-008.ethz.ch>
2013-04-08 09:22:01 +0200

    old icon moon, to align the statistics icon in course list view

---
+++ www/libs/icomoon05.04.B/lte-ie7.js
@@ -0,0 +1,51 @@
+/* Load this script using conditional IE comments if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-diamond' : '&#xe016;',
+			'icon-fire' : '&#xe017;',
+			'icon-smiley' : '&#xe000;',
+			'icon-settings' : '&#xe001;',
+			'icon-dash' : '&#xe002;',
+			'icon-cross' : '&#xe003;',
+			'icon-courses' : '&#xe004;',
+			'icon-numeric' : '&#xe005;',
+			'icon-question' : '&#xe006;',
+			'icon-clozeQuestion' : '&#xe007;',
+			'icon-checkmark2' : '&#xe008;',
+			'icon-lock' : '&#xe009;',
+			'icon-checkmark' : '&#xe00a;',
+			'icon-neutral' : '&#xe00b;',
+			'icon-loadingOld' : '&#xe00d;',
+			'icon-loading2' : '&#xe00e;',
+			'icon-lms' : '&#xe012;',
+			'icon-info' : '&#xe013;',
+			'icon-icon-move' : '&#xe014;',
+			'icon-happy' : '&#xe015;',
+			'icon-exit' : '&#xe018;',
+			'icon-enter' : '&#xe01a;',
+			'icon-drag' : '&#xe01b;',
+			'icon-equals' : '&#xe00c;',
+			'icon-bars' : '&#xe00f;',
+			'icon-arrow-up' : '&#xe010;',
+			'icon-arrow-down' : '&#xe011;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, html, c, el;
+	for (i = 0; i < els.length; i += 1) {
+		el = els[i];
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
