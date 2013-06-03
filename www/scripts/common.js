36d11b7f3639f35f5759ef022470dca76e86391f
Evangelia Mitsopoulou <emitsopo@gess-sipo-als-dock-1-008.ethz.ch>
2013-06-03 10:13:21 +0200

    activate debug mode

--- www/scripts/common.js
+++ www/scripts/common.js
@@ -77,7 +77,7 @@ var URLS_TO_LMS = [
 						backgroundImage: "",
 						logoLabel: "Estonian Defense College",
 						url: "https://eope.ksk.edu.ee/ilias/restservice/learningcards",
-						debug: "0",
+						debug: "1",
 						clientKey: ""
 					},
 					{
@@ -118,7 +118,7 @@ var URLS_TO_LMS = [
  * @param {String}messagestring, the text message to be displayed in the console
  * */
 function moblerlog(messagestring) {
-	var MOBLERDEBUG = 1;
+	var MOBLERDEBUG = 0;
 	
    if (MOBLERDEBUG === 1) {
         console.log(messagestring);
@@ -147,7 +147,7 @@ function getActiveServer(){
  * @function debugActivate
  * * */
 function debugActivate() {
-	var MOBLERDEBUG = 1;
+	var MOBLERDEBUG = 0;
 	moblerlog("debug Activate and the value of MOBLERDEBUG IS"+MOBLERDEBUG);
 	
 	if (MOBLERDEBUG === 0){
